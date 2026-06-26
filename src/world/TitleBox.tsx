6be6fc3746565e1dc605066e65618b6522c4397a
LeomharT <leo.liao.lzy@gmail.com>
2026-06-26 16:01:36 +0800

    feat(world): add 3d title and figure assets

--- src/world/TitleBox.tsx
+++ src/world/TitleBox.tsx
@@ -3,11 +3,43 @@ import {
   MeshTransmissionMaterial,
   RoundedBoxGeometry,
   Text,
+  Text3D,
+  useGLTF,
 } from "@react-three/drei";
 import { folder, useControls } from "leva";
+import type { Mesh, MeshStandardMaterial } from "three";
+import type { GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
 const GOLDRATIO = 1.618033988749;
 
+type GLTFResult = GLTF & {
+  nodes: {
+    Curve005: Mesh;
+    Curve011: Mesh;
+  };
+  materials: {
+    Material: MeshStandardMaterial;
+  };
+};
 export default function TitleBox() {
+  const { nodes } = useGLTF("/figure.glb", true) as unknown as GLTFResult;
+
+  const { metalness, roughness } = useControls({
+    "🟡 Person Figure": folder({
+      metalness: {
+        value: 0.87,
+        step: 0.01,
+        min: 0,
+        max: 1,
+      },
+      roughness: {
+        value: 0.2,
+        step: 0.01,
+        min: 0,
+        max: 1,
+      },
+    }),
+  });
+
   const { radius, ...config } = useControls({
     "🧊 Glass Box": folder({
       radius: {
@@ -34,6 +66,30 @@ export default function TitleBox() {
 
   return (
     <group position={[-6.75, 0.5, -5.75]}>
+      <Text3D font={"Inter_Bold.json"} position={[0, 0.3, 0]}>
+        LEO
+        <meshStandardMaterial
+          color={COLORS.METALLIC_GOLD}
+          metalness={metalness}
+          roughness={roughness}
+        />
+      </Text3D>
+      <group dispose={null} position={[3, 0.35, 0]} scale={30}>
+        <mesh castShadow receiveShadow geometry={nodes.Curve005.geometry}>
+          <meshStandardMaterial
+            color={COLORS.METALLIC_GOLD}
+            metalness={metalness}
+            roughness={roughness}
+          />
+        </mesh>
+        <mesh castShadow receiveShadow geometry={nodes.Curve011.geometry}>
+          <meshStandardMaterial
+            color={COLORS.METALLIC_GOLD}
+            metalness={metalness}
+            roughness={roughness}
+          />
+        </mesh>
+      </group>
       <Text
         scale={0.38}
         position={[-1.75, 0.21, -0.75]}
@@ -46,8 +102,8 @@ export default function TitleBox() {
         LEO'S DIGITAL RESUME
       </Text>
       <Text
-        scale={0.8}
-        position={[0, 0.21, 0.2]}
+        scale={0.7}
+        position={[-1.1, 0.21, 0.2]}
         rotation-x={-Math.PI / 2}
         frustumCulled={false}
         color="black"
